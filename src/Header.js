40dbd15f1d0049a99f970131a2b1c26b82493e52
chaeinPark <cip@katchers.co.kr>
2023-05-30 09:46:58 +0900

    project setting

---
+++ src/Header.js
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const IconButton = (props) => {
+  return (
+    <View style={{ paddingHorizontal: 6 }}>
+      <Ionicons name={props.name} size={24} color="black" />
+    </View>
+  );
+};
+
+export default () => {
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.title}>친구</Text>
+
+      <View style={styles.iconList}>
+        <IconButton name="search-outline" />
+        <IconButton name="person-add-outline" />
+        <IconButton name="md-musical-notes-outline" />
+        <IconButton name="ios-settings-outline" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+  },
+
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  iconList: {
+    flexDirection: "row",
+  },
+});
